0c95a199f479aab3ad9fbd7ff05ec88024d369cf
BambooCode98 <jw.thomas98@yahoo.com>
2022-07-26 00:56:49 -0500

    growing tree

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ ctx = canvas.getContext('2d');
 let rotation = 0;
 let [dx, dy, x, y] = [1,1,50,50];
 let sides = 0;
-let scaleX = 0;
-let scaleY = 0;
 
 window.addEventListener('load', () => {
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
-  ctx.lineWidth = 20;
+  ctx.lineWidth = 15;
   ctx.lineCap='round';
   ctx.strokeStyle = 'yellow';
   ctx.save();
-  ctx.translate(canvas.width/2,canvas.height/2);
-  ctx.scale(2,2);
+  // ctx.translate(canvas.width/2,canvas.height/2);
+  // ctx.scale(2,2);
   
   // ctx.rotate(0);
+  // draw(canvas.width/2,canvas.height-100,120,0,1)
 
   // lineFractals()
   
@@ -24,43 +23,58 @@ window.addEventListener('load', () => {
 
 let rotateStuff = setInterval( () => {
   sides++;
-  scaleX += 0.1;
-  if(sides === 1000) {
+  if(sides === 500) {
     clearInterval(rotateStuff)
   }
-  // if(scaleX >= 1) {
-  //   scaleX =- 0.1
-  // }
-  // if(scaleY >= 1) {
-  //   scaleY =- 0.1
-  // }
-  console.log(sides, scaleX);
+  console.log(sides);
+  draw(canvas.width/2,canvas.height-100,sides,0,1)
   
-  // for(let i = 0; i < sides; i++) {
-    // ctx.clearRect(0,0,canvas.width,canvas.height)
-    ctx.beginPath();
-    ctx.moveTo(0,0);
-    // if(x + dx < 0 || x + dx > canvas.width) {
-    //   dx = -dx;
-    // }
-    // if(y + dy < 0 || y + dy > canvas.height) {
-    //   dy = -dy
-    // }
-    // x += dx;
-    // y += dy;
-    ctx.lineTo(10,10);
-    ctx.stroke();
-  
-    ctx.rotate((Math.PI*2)/sides/0.5);
-    ctx.scale(1,0.99);
-    ctx.translate(25,25)
-    // ctx.restore();
-  // }
-},10)
+},1000)
 
-rotateStuff()
 
 function lineFractals() {
-  // lineFractals()
+  ctx.beginPath();
+  ctx.moveTo(0,0);
+  // if(x + dx < 0 || x + dx > canvas.width) {
+  //   dx = -dx;
+  // }
+  // if(y + dy < 0 || y + dy > canvas.height) {
+  //   dy = -dy
+  // }
+  // x += dx;
+  // y += dy;
+  ctx.lineTo(10,10);
+  ctx.stroke();
 
+  ctx.rotate((Math.PI*4)/sides);
+  ctx.scale(0.99,0.99);
+  ctx.translate(25,25)
+  // lineFractals()
+}
+
+
+function draw(x,y,length,angle, branchWidth) {
+  ctx.beginPath();
+  ctx.save();
+  ctx.lineWidth = branchWidth;
+  ctx.strokeStyle = "black";
+  ctx.shadowBlur = 15;
+  ctx.shadowColor = 'white';
+  ctx.translate(x,y);
+  ctx.rotate(angle*Math.PI/180);
+  ctx.moveTo(0,0);
+  ctx.lineTo(0,-length);
+  ctx.stroke();
+
+  if (length < 8) {
+    // ctx.beginPath();
+    // ctx.fillRect(x,y,10,5);
+    ctx.restore(); 
+    return;
+  }
+
+  draw(0,-length,length*0.8,+20, branchWidth/0.9)
+  draw(0,-length,length*0.8,-20, branchWidth/0.9)
+
+  ctx.restore();
 }
